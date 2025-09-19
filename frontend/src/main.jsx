51c7abc355a32bee85fb412d929ecefc941b3a88
Boyina Akhil <151818306+Boyinaakhil@users.noreply.github.com>
2025-09-19 15:47:35 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import UserContextProvider from './context/userContext'   // ✅ import correctly
+import UserContextProvider from './context/UserContext'   // ✅ import correctly
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
